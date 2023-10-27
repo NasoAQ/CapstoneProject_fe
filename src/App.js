fe43ab7476f584116ff5570df4060abeb17d78e3
naso <naso@epicode.it>
2023-10-27 20:22:03 +0200

    Template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-	return <div>PrestigeAdventures</div>;
+	return (
+		<>
+			<Navbar />
+		</>
+	);
 }
 
 export default App;
